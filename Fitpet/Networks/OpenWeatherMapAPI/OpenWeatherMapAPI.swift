9b34100939a5d573bebbb06ff4854e43a1175aa1
Jisung Park <jisungpark83@gmail.com>
2023-05-29 13:37:13 +0900

    Feat: OpenWeatherMapAPI 추가

---
+++ Fitpet/Networks/OpenWeatherMapAPI/OpenWeatherMapAPI.swift
@@ -0,0 +1,43 @@
+//
+//  OpenWeatherMapAPI.swift
+//  Fitpet
+//
+//  Created by 박지성 on 2023/05/26.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+final class OpenWeatherMapAPI {
+    static var apiKey: String?
+    
+    
+    func requestCoordinatesBy(cityName: String, countryCode: String, limit: Int) -> Single<[OWMCoordinatesDto]> {
+        return .create { observer in
+            guard let apiKey = Self.apiKey else {
+                fatalError("OpenWeatherMapAPI.apiKey is not initialized")
+            }
+            let urlString = "http://api.openweathermap.org/geo/1.0/direct?q=\(cityName),\(countryCode)&limit=\(limit)&appid=\(apiKey)"
+            
+            guard let url = URL(string: urlString) else {
+                fatalError("\(#fileID), \(#function) wrong url: \(urlString)")
+            }
+            
+            let request = AF.request(url)
+            request.responseDecodable { (response: DataResponse<[OWMCoordinatesDto], AFError>) in
+                switch response.result {
+                case .success(let dto):
+                    observer(.success(dto))
+                case .failure(let error):
+                    observer(.failure(error))
+                }
+            }
+            
+            return Disposables.create {
+                request.cancel()
+            }
+        }
+    }
+    
+}
