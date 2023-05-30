f14a6f5730a354df49362b4ab48ad50ae272e6a6
Jisung Park <jisungpark83@gmail.com>
2023-05-30 10:53:11 +0900

    fix: forecast 5개로 제한

--- Fitpet/Services/ForecastService.swift
+++ Fitpet/Services/ForecastService.swift
@@ -13,17 +13,25 @@ final class ForecastService {
     private let openWeatherMapAPI = OpenWeatherMapAPI()
     
     func requestDailyForecastOf(cityName: String, countryCode: String, timeZone: TimeZone) -> Single<DailyForecastModel> {
+        
+        let maxDailyCount = 5
+        
         return self.openWeatherMapAPI
             .requestCoordinatesBy(cityName: cityName, countryCode: countryCode, limit: 1)
             .flatMap { [weak self] dtos -> Single<OWMOneCallDto> in
                 guard let `self` = self else { return .never() }
                 guard let dto = dtos.first else {
-                    return .error(RxError.unknown)
+                    return .error(RxError.noElements)
                 }
                 return self.openWeatherMapAPI.requestOneCall(lat: dto.lat, lon: dto.lon, units: "metric")
             }
             .map { dto -> DailyForecastModel in
-                let forecasts = dto.daily.compactMap { dto -> ForecastModel? in
+                
+                let arr = maxDailyCount < dto.daily.count
+                ? Array(dto.daily[0 ..< maxDailyCount])
+                : dto.daily
+                
+                let forecasts = arr.compactMap { dto -> ForecastModel? in
                     guard let weather = dto.weather.first else {
                         return nil
                     }
